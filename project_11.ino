8c5caee12d368e4fa0b0e1a26c7260c211f23253
Ravikrishnan05 <134152503+Ravikrishnan05@users.noreply.github.com>
2023-05-22 22:07:04 +0530

    Add files via upload

---
+++ project_11.ino
@@ -0,0 +1,28 @@
+// C++ code
+//basic program to connect and use 2 sensors 
+#define pot A0
+#define button 6
+void setup()
+{
+  pinMode(pot, INPUT);
+  pinMode(button, INPUT);
+  Serial.begin(9600);
+  
+}
+void loop()
+{
+  bool bdata ;  //
+  int pdata = 0;
+  bdata=digitalRead(button);
+  pdata=analogRead(pot);
+  
+  Serial.print("pot data:");
+  Serial.print(pdata);
+  Serial.print(" button data:");
+  Serial.print(bdata);
+  Serial.print("\n");
+  
+  delay(2000);
+  
+  
+}
